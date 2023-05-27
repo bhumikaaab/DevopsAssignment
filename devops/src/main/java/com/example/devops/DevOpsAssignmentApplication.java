b87315d6ee231504ee0e3fd6bb3763a4696c84a0
bhumikaaaB <127742509+bhumikaaaB@users.noreply.github.com>
2023-05-27 16:24:16 +0530

    Update DevOpsAssignmentApplication.java

--- devops/src/main/java/com/example/devops/DevOpsAssignmentApplication.java
+++ devops/src/main/java/com/example/devops/DevOpsAssignmentApplication.java
@@ -3,7 +3,7 @@ package com.example.devops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = DevOpsAssignmentApplication.class )
+@SpringBootApplication
 public class DevOpsAssignmentApplication {
 
 	public static void main(String[] args) {
